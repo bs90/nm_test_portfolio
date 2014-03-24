265fcb6819145b0d52190726fc955c3762f11ae6
bs90 <trongtranba@pm3.local>
2014-03-24 13:37:53 +0900

    js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 $(function(){
-  $(".dmenu").click(function(){
+  $(".clicktoscroll").click(function(){
     $("#allweb").animate({
-      left: - 1020*$(this).attr("number")
+      left: - $(this).attr("number")*1020
     },500);
   });
+  $("a[rel=ds_group]").fancybox({
+     'transitionIn'    : 'none',
+     'transitionOut'   : 'none',
+     'titlePosition'   : 'over',
+     'titleFormat'   : function(title, currentArray, currentIndex, currentOpts) {
+     return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+   }
+ });
+  $("a[rel=pt_group]").fancybox({
+     'transitionIn'    : 'none',
+     'transitionOut'   : 'none',
+     'titlePosition'   : 'over',
+     'titleFormat'   : function(title, currentArray, currentIndex, currentOpts) {
+     return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+   }
+ });
 });
